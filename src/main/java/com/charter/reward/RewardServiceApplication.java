82937d3f67826884ee343960364e4212180abbc4
ramprasad26595 <43598379+ramprasad26595@users.noreply.github.com>
2026-07-01 16:09:05 +0530

    refactor and format code based on review comments

---
+++ src/main/java/com/charter/reward/RewardServiceApplication.java
@@ -0,0 +1,16 @@
+package com.charter.reward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application entry point for the reward service.
+ */
+@SpringBootApplication
+public class RewardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardServiceApplication.class, args);
+	}
+
+}
